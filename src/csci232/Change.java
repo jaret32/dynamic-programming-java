d83e60d6c98f9389e95851a80fefec7f6f696dd1
Jaret Boyer <jaret.c.boyer@gmail.com>
2018-04-23 15:03:43 -0600

    initial commit with bare classes

---
+++ src/csci232/Change.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csci232;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jaret
+ */
+public class Change {
+    
+    public static ArrayList<Integer> get() {
+        ArrayList<Integer> coins = new ArrayList();
+        return coins;
+    }
+    
+}

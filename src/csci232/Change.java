edda2ab0615980f966aac0282b873b50d5cf088f
Jaret Boyer <jaret.c.boyer@gmail.com>
2018-04-23 19:18:09 -0600

    added comments for the dynamic programming solution

--- src/csci232/Change.java
+++ src/csci232/Change.java
@@ -7,39 +7,51 @@ package csci232;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Jaret
- */
+
 public class Change {
     
+    // dynamic programming change algorithm
     public static ArrayList<Integer> get(int[] coins, int amount) {
+        // table for minimum number of coins for each value up to amount
         int[] numCoins = new int[amount + 1];
+        // table for the last coin used for each value up to amount
         int[] lastCoin = new int[amount + 1];
         numCoins[0] = 0;
         lastCoin[0] = 0;
         
+        // iterate through each value up to amount
         for (int i = 1; i <= amount; i++) {
             int min = Integer.MAX_VALUE;
             int used = 0;
+            // iterate through coins
             for (int coin: coins) {
+                // if the coin is less than the value
                 if (coin <= i) {
+                    // and using the coin results in using less coins overall
                     if (1 + numCoins[i - coin] < min) {
+                        // update the min number of coins
                         min = 1 + numCoins[i - coin];
+                        // and the last coin used
                         used = coin;
                     }
                 }
             }
+            // set min number of coins for the value
             numCoins[i] = min;
+            // and the last coin used for the value
             lastCoin[i] = used;
         }
         
+        // create an array list to store used coins
         ArrayList<Integer> coinsUsed = new ArrayList();
         
+        // get first coin
         int last = lastCoin[amount];
         int remaining = amount;
         
+        // while there is more change
         while (last != 0) {
+            // get next coin and redo
             coinsUsed.add(last);
             remaining = remaining - last;
             last = lastCoin[remaining];
@@ -48,6 +60,7 @@ public class Change {
         return coinsUsed;
     }
     
+    // utility function to print the two tables for testing
     public static void printArray(int[] arr) {
         String output = "[ ";
         for (int i = 0; i < arr.length; i++) {
@@ -62,6 +75,7 @@ public class Change {
         System.out.println(output);
     }
     
+    // utility function to print the coin list for testing
     public static void printList(ArrayList list) {
         String output = "[ ";
         for (int i = 0; i < list.size(); i++) {

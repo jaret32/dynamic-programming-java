a039584d6c2c43b4fc06202502fcf65a9b194f8f
kaylaawheelerr <35355568+kaylaawheelerr@users.noreply.github.com>
2018-04-23 18:54:03 -0600

    added another test case

--- test/csci232/ChangeTest.java
+++ test/csci232/ChangeTest.java
@@ -20,7 +20,7 @@ public class ChangeTest {
     }
 
     @Test
-    public void testGet() {
+    public void testGet1() {
         int[] coins = {1, 5, 10, 12, 25};
         int change = 24;
         ArrayList<Integer> expResult = new ArrayList();
@@ -31,12 +31,24 @@ public class ChangeTest {
     }
     
     @Test
-    public void testGet1() {
+    public void testGet2() {
         int[] coins = {};
         int change = 0;
         ArrayList<Integer> expResult = new ArrayList();
         Exception e = assertThrows(IllegalArgumentException.class, () -> {  Change.get(coins,change); } );
 		assertEquals("Array of size 0 is not allowed", e.getMessage());
+    }
+    
+    @Test
+    public void testGet3() {
+        int[] coins = {1, 5, 10, 12, 25};
+        int change = 55;
+        ArrayList<Integer> expResult = new ArrayList();
+        expResult.add(5);
+        expResult.add(25);
+        expResult.add(25);
+        ArrayList<Integer> result = Coin.get(coins, change);
+        assertEquals(expResult, result);
     }
     
 }
